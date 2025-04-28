116a5a6786416022d5375e719a0c9f8f47963b6f
Mihail <m.volkov@stv.dunice.net>
2025-04-28 18:37:46 +0300

    Compete

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # URL для админ-панели
     path('', include('blog.urls', namespace='blog')),  # Подключение маршрутов приложения blog
     path('pages/', include('pages.urls', namespace='pages')),  # Подключение маршрутов приложения pages
 ]
